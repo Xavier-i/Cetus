a21484c1abdec04468f9e74fbf114db041b39706
Xavier Li <lihaohan19970724@gmail.com>
2017-08-06 09:40:41 -0500

    added using namespace arma;

--- src/LinearModel/LinearRegression.cc
+++ src/LinearModel/LinearRegression.cc
@@ -2,11 +2,13 @@
 #include <armadillo>
 #include <iostream>
 
-LinearRegression::LinearRegression(arma::mat *x, arma::vec *y, arma::uword m)
-    : y{y}, m{m}, trained{false} {
+using namespace arma;
+
+LinearRegression::LinearRegression(mat *x, vec *y, uword m)
+    : x{x}, y{y}, m{m}, trained{false} {
       //Create Bias Layer and append at the end of  x
-      arma::mat bias = arma::ones<arma::mat>(m,2);
-      this->x = x->arma::insert_cols(x->n_cols, bias);
+      mat bias = ones<mat>(m,2);
+      this->x->insert_cols(x->n_cols, bias);
     }
 
 LinearRegression::~LinearRegression() {
@@ -22,17 +24,17 @@ void LinearRegression::AddData(double x[], double y[]) {
 }
 
 void LinearRegression::Train() {
-  arma::mat xtx = (this->x->t() * (*this->x));
+  mat xtx = (this->x->t() * (*this->x));
   // Check if xtx is full-rank matrix
   if (rank(xtx) == this->m) {
-    this->w = new arma::mat(inv(xtx) * this->x->t() * *y);
+    this->w = new mat(inv(xtx) * this->x->t() * *y);
     this->trained = true;
   } else {
     std::cerr << "you have to regularize your data set" << std::endl;
   }
 }
 
-double LinearRegression::Predict(arma::vec *x) {
+double LinearRegression::Predict(vec *x) {
   if (!this->trained) {
     std::cerr << "This model hasn't been trained" << std::endl;
     return 0.0;

b194c03ad92ca342a7f98fa50e7d3563228b9a67
Xavier Li <lihaohan19970724@gmail.com>
2017-08-07 13:27:03 -0500

    Update testing script

--- src/LinearModel/test.cc
+++ src/LinearModel/test.cc
@@ -30,7 +30,25 @@ void testSucc() {
   std::cout << A->Predict(q) << std::endl;
 }
 
+void testSucc2() {
+  mat x = mat("3 0; 0 4;2 5;0 0; 5 2; 7 0; 0 8");
+  vec y = vec("3 8 12 0 9 7 16");
+  LinearRegression *A = new LinearRegression(x, y);
+  A->Train(false, 0.1, 10000000);
+  vec p = vec("3 0");
+  std::cout << A->Predict(p) << std::endl;
+  vec q = vec("0 8");
+  std::cout << A->Predict(q) << std::endl;
+  mat ex = mat("0 0;");
+  vec ey = vec("0");
+
+  A->AddData(ex, ey);
+  A->Train(false, 0.01, 1000000);
+  std::cout << A->Predict(q) << std::endl;
+}
+
 main() {
   testFailed();
   testSucc();
+  testSucc2();
 }

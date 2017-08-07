dd0d6038b6a6b7afe98aa11ca336ee9babb9607f
Xavier Li <lihaohan19970724@gmail.com>
2017-08-06 19:46:19 -0500

    Fixed Bugs

--- src/LinearModel/LinearRegression.cc
+++ src/LinearModel/LinearRegression.cc
@@ -5,22 +5,31 @@
 
 using namespace arma;
 
-LinearRegression::LinearRegression(mat &x, vec &y)
-    : x{x}, y{y}, trained{false} {
+LinearRegression::LinearRegression(mat &x, vec &y) : trained{false} {
   assert(x.n_rows == y.n_rows);
 
   // Create Bias Layer and append at the end of  x
-  mat bias = ones<mat>(y.n_rows, 2);
-  this->x.insert_cols(x.n_cols, bias);
+  mat bias = ones<mat>(x.n_rows, 1);
+  mat inputX = x;
+  inputX.insert_cols(x.n_cols, bias);
+  this->x = inputX;
+  vec inputY = y;
+  this->y = inputY;
+  // this->x = inputX;
 }
 
 LinearRegression::~LinearRegression() {}
 
 void LinearRegression::AddData(mat &extraX, vec &extraY) {
   assert(extraX.n_rows == extraY.n_rows);
-  assert(extraX.n_rows == this->x.n_rows);
+  assert((extraX.n_cols + 1) == this->x.n_cols);
+
   this->trained = false;
-  this->x.insert_rows(this->x.n_rows, extraX);
+  // Add Bias Layer to latest added input
+  mat bias = ones<mat>(extraX.n_rows, 1);
+  mat inputX = extraX;
+  inputX.insert_cols(inputX.n_cols, bias);
+  this->x.insert_rows(this->x.n_rows, inputX);
   this->y.insert_rows(this->y.n_rows, extraY);
 }
 
@@ -42,5 +51,8 @@ double LinearRegression::Predict(vec &x) {
     std::cerr << "This model hasn't been trained" << std::endl;
     return 0.0;
   }
-  return (this->w * x).eval()(0, 0);
+  vec bias = vec("1");
+  vec input = x;
+  input.insert_rows(x.n_rows, bias);
+  return (input.t() * this->w).eval()(0, 0);
 }

894cbfdd923c4bc8ae8f51286edc07770aba83a3
Xavier Li <lihaohan19970724@gmail.com>
2017-08-06 19:46:32 -0500

    enhanced testing scripts

--- src/LinearModel/test.cc
+++ src/LinearModel/test.cc
@@ -5,11 +5,32 @@
 
 using namespace arma;
 
-main() {
+void testFailed() {
   mat x = mat("1 0 0; 0 0 1");
   vec y = vec("0 1");
   LinearRegression *A = new LinearRegression(x, y);
   A->Train();
   vec p = vec("1 0 0");
+}
+
+void testSucc() {
+  mat x = mat("3 0; 0 4;2 5; 5 2");
+  vec y = vec("3 8 12 9 ");
+  LinearRegression *A = new LinearRegression(x, y);
+  A->Train();
+  vec p = vec("3 0");
   std::cout << A->Predict(p) << std::endl;
+  vec q = vec("0 8");
+  std::cout << A->Predict(q) << std::endl;
+  mat ex = mat("0 0;");
+  vec ey = vec("0");
+
+  A->AddData(ex, ey);
+  A->Train();
+  std::cout << A->Predict(q) << std::endl;
+}
+
+main() {
+  testFailed();
+  testSucc();
 }

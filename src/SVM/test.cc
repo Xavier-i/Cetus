579453a48f620019882eb74b142135fd1e863e46
Xavier Li <lihaohan19970724@gmail.com>
2017-09-09 15:08:15 -0400

    Added test file

---
+++ src/SVM/test.cc
@@ -0,0 +1,23 @@
+#include "SupportVectorMachine.h"
+#include <armadillo>
+#include <assert.h>
+#include <iostream>
+using namespace std;
+using namespace arma;
+
+void test1() {
+  mat x = mat("2 2; 3 3; 4 4; -1 2; -10 8; 8 9; -4 -4;1 -3; 8 1; 3 -4; 1 -1; "
+              "-1 1; 4 -6");
+  vec y = vec("1 1 1 -1 -1 1 -1 -1 1 -1 -1 -1 -1 ");
+  SupportVectorMachine *A = new SupportVectorMachine(x, y, 1.0, LINEAR);
+  A->Train();
+
+  vec p = vec("3 0");
+  vec q = vec("5 -5");
+  vec f = vec("3 7");
+  std::cout << A->Predict(p) << std::endl;
+  std::cout << A->Predict(q) << std::endl;
+  std::cout << A->Predict(f) << std::endl;
+}
+
+int main() { test1(); }

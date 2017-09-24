6c478405c7503d8770f059ab5f2071637908440e
Xavier Li <lihaohan19970724@gmail.com>
2017-09-24 15:42:46 -0400

    Updated Input Class

---
+++ src/Input/Input.cc
@@ -0,0 +1,20 @@
+#include <armadillo>
+#include "Input.h"
+#include <string>
+
+using namespace arma;
+
+/*bool ModelInput::LoadFromFile(std::string path, FileType inputType, Object goal) {
+  switch (goal) {
+  case TARGET: {
+    bool result = target.load(path, inputType);
+    // check the target data
+    return result;
+  }
+  case DATA:
+    return data.load(path, inputType);
+  default:
+    return false;
+  }
+}
+*/
--- src/Input/Input.h
+++ src/Input/Input.h
@@ -1,14 +1,25 @@
 #ifndef CETUS_INPUT_H_
 #define CETUS_INPUT_H_
 #include <armadillo>
+#include <string>
+
 enum ModelType { LINEARREGRESSION, LOGISTICREGRESSION, SVM }; /* model_type */
+enum FileType { CSV };
+enum Object { TARGET, DATA };
 
 struct ModelInput {
+  ModelInput(ModelType modelType) : type{modelType} {}
+  ~ModelInput() {}
+
+  // Load From File
+  //bool LoadFromFile(std::string path, FileType inputType, Object goal);
+
+  //*ModelInput
+
   ModelType type;
 
   arma::mat data;
   arma::vec target;
 };
-
 
 #endif
